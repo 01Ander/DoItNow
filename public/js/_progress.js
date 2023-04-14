ba4bde598c5ef3b3552b161390987d8006a8e8d4
01Ander <arinconayala@hotmail.com>
2023-04-14 11:42:17 -0500

    added progressBar's styles

--- public/js/_progress.js
+++ public/js/_progress.js
@@ -5,11 +5,11 @@ function progress() {
   const { tasksDoneLength, tasksNotDoneLength, tasksLength } = readTasks();
   const progressContainer = document.querySelector(".progress__bar");
   const progressText = document.querySelector(".progress__text");
+  const progressMainContainer = document.querySelector('.progress__bar--container')
   progressText.classList.add('header__subtitle')
   const progressWidth = (tasksDoneLength / tasksLength) * 100;
   if (progressWidth !== 100) {
     progressContainer.style.width = `${progressWidth}%`;
-    progressContainer.classList.remove("progress__bar--completed");
   } else {
     progressContainer.style.width = `${progressWidth}%`;
     progressContainer.classList.add("progress__bar--completed");
@@ -17,10 +17,13 @@ function progress() {
 
   if (tasksLength === 0) {
     progressText.textContent = "You have no tasks";
+    progressMainContainer.classList.add('inactive')
   } else if (progressWidth === 100) {
     progressText.textContent = "You have completed all your tasks";
+    progressMainContainer.classList.remove('inactive')
   } else {
     progressText.textContent = `You have completed ${tasksDoneLength} of ${tasksLength} tasks`;
+    progressMainContainer.classList.remove('inactive')
   }
 }
 
